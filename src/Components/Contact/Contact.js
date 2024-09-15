044e02b2f9d9f1342dd4e21ed54ca1d5a28e3255
Imtiaz <axubair9@gmail.com>
2024-09-16 01:07:32 +0600

    Contact page responsive complete!

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -94,11 +94,11 @@ const Contact = () => {
     <div>
       <HelmetHook pageName={"Mahmida - Contact"} />
       <NavBar />
-      <div className="w-[85%] m-auto">
-        <div className="grid grid-cols-5 gap-6 mt-[3rem]">
-          <div className="col-span-2 flex flex-col justify-between">
-            <div className="box-card p-4 flex flex-col gap-6">
-              <div className="inside-border-box p-8 flex items-center gap-4">
+      <div className="w-[95%] sm:w-[85%] m-auto">
+        <div className="grid grid-cols-1 xl:grid-cols-5 gap-3 sm:gap-6 mt-[1rem] sm:mt-[3rem]">
+          <div className="xl:col-span-2 flex flex-col justify-between gap-4 order-2 xl:order-none">
+            <div className="box-card p-3 sm:p-4 grid grid-cols-1 gap-3 sm:gap-6">
+              <div className="inside-border-box p-4 sm:p-8 flex items-center gap-4">
                 <div
                   style={{
                     background:
@@ -106,25 +106,25 @@ const Contact = () => {
                     backdropFilter: "blur(15px)",
                     border: ".75px solid rgba(251, 251, 251, 0.1)",
                   }}
-                  className="p-5 flex items-center gap-4"
+                  className="p-3 sm:p-5 flex items-center gap-4"
                 >
-                  <CiMail
-                    style={{ width: "32px", height: "32px", color: "#00aaff" }}
-                  />
+                  <CiMail className="sm:text-[32px] text-[25px]" />
                 </div>
 
-                <div className="text-left flex flex-col gap-1">
-                  <p className="text-accent text-[16px] tracking-[0.5px] font-medium">
+                <div className="text-left flex flex-col gap-[0.15rem] sm:gap-1">
+                  <p className="text-accent text-[14px] sm:text-[16px] tracking-[0.5px] font-medium">
                     MAIL US
                   </p>
-                  <p className="text-primary text-[14px]">info@bluebase.com</p>
-                  <p className="text-primary text-[14px]">
+                  <p className="text-primary text-[11px] sm:text-[14px]">
+                    info@bluebase.com
+                  </p>
+                  <p className="text-primary text-[11px] sm:text-[14px]">
                     {" "}
                     info@bluebase2.com
                   </p>
                 </div>
               </div>
-              <div className="inside-border-box p-8 flex items-center gap-4">
+              <div className="inside-border-box p-4 sm:p-8 flex items-center gap-4">
                 <div
                   style={{
                     background:
@@ -132,24 +132,21 @@ const Contact = () => {
                     backdropFilter: "blur(15px)",
                     border: ".75px solid rgba(251, 251, 251, 0.1)",
                   }}
-                  className="p-5 flex items-center gap-4"
+                  className="p-3 sm:p-5 flex items-center gap-4"
                 >
-                  <MdOutlineWatchLater
-                    style={{ width: "32px", height: "32px", color: "#00aaff" }}
-                  />
+                  <MdOutlineWatchLater className="sm:text-[32px] text-[25px]" />
                 </div>
 
-                <div className="text-left flex flex-col gap-1">
-                  <p className="text-accent text-[16px] tracking-[0.5px] font-medium">
+                <div className="text-left flex flex-col gap-[0.15rem] sm:gap-1">
+                  <p className="text-accent text-[14px] sm:text-[16px] tracking-[0.5px] font-medium">
                     WORKING HOURS
                   </p>
-                  <p className="text-primary text-[14px]">
+                  <p className="text-primary text-[11px] sm:text-[14px]">
                     10.00 PM to 06.00 AM
                   </p>
-                  {/* <p className="text-primary text-[14px]"> +1 504-749-5456</p> */}
                 </div>
               </div>
-              <div className="inside-border-box p-8 flex items-center gap-4">
+              <div className="inside-border-box p-4 sm:p-8 flex items-center gap-4">
                 <div
                   style={{
                     background:
@@ -157,32 +154,33 @@ const Contact = () => {
                     backdropFilter: "blur(15px)",
                     border: ".75px solid rgba(251, 251, 251, 0.1)",
                   }}
-                  className="p-5 flex items-center gap-4"
+                  className="p-3 sm:p-5 flex items-center gap-4"
                 >
-                  <IoLocationOutline
-                    style={{ width: "32px", height: "32px", color: "#00aaff" }}
-                  />
+                  <IoLocationOutline className="sm:text-[32px] text-[25px]" />
                 </div>
 
-                <div className="text-left flex flex-col gap-1">
-                  <p className="text-accent text-[16px] tracking-[0.5px] font-medium">
+                <div className="text-left flex flex-col gap-[0.15rem] sm:gap-1">
+                  <p className="text-accent text-[14px] sm:text-[16px] tracking-[0.5px] font-medium">
                     LOCATION
                   </p>
-                  <p className="text-primary text-[14px]">
+                  <p className="text-primary text-[11px] sm:text-[14px]">
                     22 Baker Street, Texas United States
                   </p>
-                  <p className="text-primary text-[14px]"> W1U 3BW</p>
+                  <p className="text-primary text-[11px] sm:text-[14px]">
+                    {" "}
+                    W1U 3BW
+                  </p>
                 </div>
               </div>
             </div>
 
-            <div className="box-card p-4">
+            <div className="box-card p-4 hidden xl:block">
               <div className="inside-border-box p-4 text-left">
                 <p className="text-[20px] tracking-[0.5px] mb-4 font-medium text-center">
                   SOCIAL INFO
                 </p>
 
-                <div className="flex items-center justify-center flex-wrap gap-8">
+                <div className="flex items-center justify-center gap-6 lg:gap-8">
                   <Link
                     style={{
                       background:
@@ -251,9 +249,9 @@ const Contact = () => {
               </div>
             </div>
           </div>
-          <div className="col-span-3">
-            <div className="box-card p-6">
-              <div className="inside-border-box p-4 flex items-stretch gap-2">
+          <div className="xl:col-span-3 order-1 xl:order-none">
+            <div className="box-card p-3 sm:p-6">
+              <div className="inside-border-box p-2 sm:p-4 flex items-stretch gap-1 sm:gap-2">
                 <div
                   style={{
                     background:
@@ -261,7 +259,7 @@ const Contact = () => {
                     backdropFilter: "blur(26.25249671936035px)",
                     border: ".75px solid rgb(252 252 252 / 10%)",
                   }}
-                  className="py-3 px-4 text-[13px] tracking-[0.3px] w-fit"
+                  className="py-3 px-4 text-[11px] sm:text-[13px] tracking-[0.4px] sm:tracking-[0.3px] w-fit"
                 >
                   Contact
                 </div>
@@ -275,7 +273,7 @@ const Contact = () => {
                   }}
                   className="marquee-cus w-full flex items-center"
                 >
-                  <p className="text-[12px] tracking-[0.5px] font-[300] text-accent">
+                  <p className="text-[11px] tracking-[0.5px] font-[300] text-accent">
                     ⌘ It is a long established fact that a reader will
                     distracted by the readable content. ⌘
                   </p>
@@ -292,7 +290,7 @@ const Contact = () => {
                   transition: "0.3s ease",
                   height: "100%",
                 }}
-                className="flex flex-col gap-6 p-4 mt-4"
+                className="flex flex-col gap-3 sm:gap-6 p-3 sm:p-4 mt-2 sm:mt-4"
               >
                 <div>
                   <label htmlFor="Name"> </label>
@@ -306,11 +304,11 @@ const Contact = () => {
                     }}
                     placeholder="Full Name*"
                     type="text"
-                    className={`w-full focus:outline-0 h-full p-[1rem] text-accent ${
+                    className={`w-full focus:outline-0 h-full p-[0.9rem] sm:p-[1rem] text-accent ${
                       errors.nameError
                         ? "input-focus-border-error"
                         : "input-focus-border"
-                    } placeholder:text-[15px]`}
+                    } placeholder:text-[13px] sm:placeholder:text-[15px] text-[13px] sm:text-[15px]`}
                   />
                 </div>
                 <div>
@@ -323,13 +321,13 @@ const Contact = () => {
                       background:
                         "linear-gradient(to right, rgba(0, 170, 255, 0.01), rgba(0, 170, 255, 0.05))",
                     }}
-                    placeholder="Email"
+                    placeholder="Email*"
                     type="email"
-                    className={`w-full focus:outline-0 h-full p-[1rem] text-accent ${
+                    className={`w-full focus:outline-0 h-full p-[0.9rem] sm:p-[1rem] text-accent ${
                       errors.emailError
                         ? "input-focus-border-error"
                         : "input-focus-border"
-                    } placeholder:text-[15px]`}
+                    } placeholder:text-[13px] sm:placeholder:text-[15px] text-[13px] sm:text-[15px]`}
                   />
                 </div>
                 <div>
@@ -342,13 +340,13 @@ const Contact = () => {
                       background:
                         "linear-gradient(to right, rgba(0, 170, 255, 0.01), rgba(0, 170, 255, 0.05))",
                     }}
-                    placeholder="Subject"
+                    placeholder="Subject*"
                     type="text"
-                    className={`w-full focus:outline-0 h-full p-[1rem] text-accent ${
+                    className={`w-full focus:outline-0 h-full p-[0.9rem] sm:p-[1rem]text-accent ${
                       errors.subjectError
                         ? "input-focus-border-error"
                         : "input-focus-border"
-                    } placeholder:text-[15px]`}
+                    } placeholder:text-[13px] sm:placeholder:text-[15px] text-[13px] sm:text-[15px]`}
                   />
                 </div>
                 <div>
@@ -362,29 +360,29 @@ const Contact = () => {
                         "linear-gradient(to right, rgba(0, 170, 255, 0.01), rgba(0, 170, 255, 0.05))",
                     }}
                     rows="8"
-                    placeholder="Message"
+                    placeholder="Message*"
                     type="text"
-                    className={`w-full focus:outline-0 h-full p-[1rem] text-accent ${
+                    className={`w-full focus:outline-0 h-full p-[0.9rem] sm:p-[1rem] text-accent ${
                       errors.messageError
                         ? "input-focus-border-error"
                         : "input-focus-border"
-                    } placeholder:text-[15px] resize-none`}
+                    } placeholder:text-[13px] sm:placeholder:text-[15px] text-[13px] sm:text-[15px] resize-none`}
                   ></textarea>
                 </div>
                 <input
                   style={{ border: ".75px solid rgba(252, 252, 252, 0.05)" }}
-                  className={`btn bg-[#031B29] border-[#272727] py-[12px] px-[30px] text-[16px] font-[500] rounded-[4px] hover:bg-primary duration-500 hover:text-[#000] shadow-none ${
+                  className={`btn bg-[#031B29] border-[#272727] py-[12px] px-[30px] text-[13px] sm:text-[16px] font-[500] rounded-[4px] hover:bg-primary duration-500 hover:text-[#000] shadow-none ${
                     activeButton && "btn-disabled"
                   }`}
                   type="submit"
-                  value="SUBMIT NOW"
+                  value="SEND"
                 />
               </form>
             </div>
           </div>
         </div>
 
-        <div className="flex mt-5 justify-between gap-5">
+        <div className="grid grid-cols-2 xl:grid-cols-4 mt-3 sm:mt-5 justify-between gap-3 sm:gap-5">
           <HomeBlog />
           <ServiceOffering />
           <SocialLinks />
