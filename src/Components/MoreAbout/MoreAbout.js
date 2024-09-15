bcde34f771adb261ef911592d980f7c568f93736
Imtiaz <axubair9@gmail.com>
2024-09-16 02:12:03 +0600

    More about page responsive complete!

--- src/Components/MoreAbout/MoreAbout.js
+++ src/Components/MoreAbout/MoreAbout.js
@@ -1,12 +1,17 @@
 import React, { useEffect } from "react";
 import NavBar from "../NavBar/NavBar";
 import userPic from "../../Images/me.jpg";
-import { FaFacebookSquare, FaInstagramSquare } from "react-icons/fa";
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaLinkedin,
+} from "react-icons/fa";
 import Footer from "../Footer/Footer";
 import { Link } from "react-router-dom";
 import { CiLinkedin } from "react-icons/ci";
 import { LiaBehanceSquare } from "react-icons/lia";
 import HelmetHook from "../../Hooks/HelmetHook";
+import { AiOutlineBehanceSquare } from "react-icons/ai";
 
 const MoreAbout = () => {
   const aboutData = {
@@ -84,20 +89,20 @@ const MoreAbout = () => {
     ],
   };
 
-  useEffect(() => {
-    window.scrollTo({
-      top: "2rem",
-    });
-  }, []);
+  // useEffect(() => {
+  //   window.scrollTo({
+  //     top: "2rem",
+  //   });
+  // }, []);
 
   return (
     <div>
       <HelmetHook pageName={"Mahmida - More About"} />
       <NavBar />
 
-      <div className="w-[85%] m-auto">
-        <div className="flex gap-[1.5rem] mt-[3rem] relative">
-          <div className="w-[30%] h-[90vh] sticky top-0 bottom-0 right-0 left-0 box-card p-6">
+      <div className="w-[95%] sm:w-[85%] m-auto">
+        <div className="grid xl:grid-cols-10 gap-[1rem] mt-[1rem] sm:mt-[3rem] relative">
+          <div className="xl:col-span-3 xl:h-[90vh] h-fit xl:sticky xl:top-0 xl:bottom-0 xl:right-0 xl:left-0 box-card p-3 sm:p-6">
             <div
               style={{
                 background:
@@ -106,22 +111,22 @@ const MoreAbout = () => {
                 boxShadow: "0 1px 2px #00000040",
                 height: "100%",
               }}
-              className="single-box p-4 flex flex-col gap-8"
+              className="single-box p-2 sm:p-4 flex flex-col gap-5 sm:gap-8"
             >
-              <div className="mb-1">
-                <img className="object-cover" src={userPic} alt="mb-1" />
+              <div className="mb-0 sm:mb-1">
+                <img className="object-cover" src={userPic} alt="" />
               </div>
 
               <div>
-                <p className="text-[27px] font-semibold tracking-[0.5px] text-accent">
+                <p className="text-[23px] sm:text-[27px] font-semibold tracking-[0.5px] text-accent">
                   Mahmida Masum
                 </p>
-                <p className="text-primary text-[13px] font-[300] tracking-[0.5px]">
+                <p className="text-primary text-[11px] sm:text-[13px] font-[300] tracking-[0.5px]">
                   @mumu.mahmida
                 </p>
               </div>
 
-              <div className="flex ietms-center flex-wrap justify-center gap-6">
+              <div className="flex ietms-center justify-center gap-4">
                 <Link
                   style={{
                     background:
@@ -129,11 +134,9 @@ const MoreAbout = () => {
                     backdropFilter: "blur(15px)",
                     border: ".75px solid rgba(251, 251, 251, 0.1)",
                   }}
-                  className="p-4"
+                  className="p-2 sm:p-4"
                 >
-                  <LiaBehanceSquare
-                    style={{ width: "20px", height: "20px", color: "#00aaff" }}
-                  />
+                  <AiOutlineBehanceSquare className="text-[18px] sm:text-[20px]" />
                 </Link>
 
                 <Link
@@ -143,11 +146,9 @@ const MoreAbout = () => {
                     backdropFilter: "blur(15px)",
                     border: ".75px solid rgba(251, 251, 251, 0.1)",
                   }}
-                  className="p-4"
+                  className="p-2 sm:p-4"
                 >
-                  <FaFacebookSquare
-                    style={{ width: "20px", height: "20px", color: "#00aaff" }}
-                  />
+                  <FaFacebookSquare className="text-[18px] sm:text-[20px]" />
                 </Link>
 
                 <Link
@@ -157,11 +158,9 @@ const MoreAbout = () => {
                     backdropFilter: "blur(15px)",
                     border: ".75px solid rgba(251, 251, 251, 0.1)",
                   }}
-                  className="p-4"
+                  className="p-2 sm:p-4"
                 >
-                  <FaInstagramSquare
-                    style={{ width: "20px", height: "20px", color: "#00aaff" }}
-                  />
+                  <FaInstagramSquare className="text-[18px] sm:text-[20px]" />
                 </Link>
 
                 <Link
@@ -171,105 +170,92 @@ const MoreAbout = () => {
                     backdropFilter: "blur(15px)",
                     border: ".75px solid rgba(251, 251, 251, 0.1)",
                   }}
-                  className="p-4"
+                  className="p-2 sm:p-4"
                 >
-                  <CiLinkedin
-                    style={{ width: "20px", height: "20px", color: "#00aaff" }}
-                  />
+                  <FaLinkedin className="text-[18px] sm:text-[20px]" />
                 </Link>
               </div>
 
               <Link
                 to="/contact"
                 style={{ border: ".75px solid rgba(252, 252, 252, 0.05)" }}
-                className="btn bg-[#031B29] border-[#272727] py-[12px] px-[30px] text-[16px] font-[500] rounded-[4px] hover:bg-primary duration-500 hover:text-[#000] shadow-none"
+                className="btn bg-[#031B29] border-[#272727] py-[12px] px-[30px] text-[13px] sm:text-[15px] font-[500] rounded-[4px] hover:bg-primary duration-500 hover:text-[#000] shadow-none"
               >
                 Let's talk
               </Link>
             </div>
           </div>
-          <div className="w-[70%] border box-card p-4">
-            <div className="inside-border-box text-left p-8 flex flex-col gap-12">
+          <div className="xl:col-span-7 border box-card p-3 sm:p-4">
+            <div className="inside-border-box text-left p-5 sm:p-8 flex flex-col gap-8 sm:gap-12">
               <div>
-                <p className="text-[#fff] font-medium text-[20px] tracking-[0.5px] mb-4">
+                <p className="text-[#fff] font-medium text-[17px] sm:text-[20px] tracking-[0.5px] mb-2 sm:mb-4">
                   ABOUT ME
                 </p>
-                <p className="text-primary text-[14px] tracking-[0.4px]">
+                <p className="text-primary text-[12px] sm:text-[14px] tracking-[0.4px]">
                   {aboutData.detailsInWide}
                 </p>
               </div>
-              <div className="flex flex-col gap-4">
-                <p className="text-[#fff] font-medium text-[20px] tracking-[0.5px]">
+
+              <div className="flex flex-col gap-3 sm:gap-4">
+                <p className="text-[#fff] font-medium text-[17px] sm:text-[20px] tracking-[0.5px]">
                   EXPERIENCE
                 </p>
                 {aboutData?.experienceData?.map((eachData) => (
                   <div key={eachData._id} className="mb-2">
-                    <p className="text-primary mb-[5px]">{eachData.year}</p>
-                    <p className="text-[#4D66D0] font-medium text-[20px]">
+                    <p className="text-primary text-[13px] sm:text-[15px] mb-[3px] sm:mb-[5px]">
+                      {eachData.year}
+                    </p>
+                    <p className="text-[#4D66D0] font-medium text-[18px] sm:text-[20px]">
                       {eachData.heading}
                     </p>
-                    <p className="text-primary text-[12px] mt-1">
+                    <p className="text-primary text-[10px] sm:text-[12px] sm:mt-1">
                       {eachData.institution}
                     </p>
-                    <p className="text-primary text-[14px] mt-1">
+                    <p className="text-primary text-[12px] sm:text-[14px] mt-1">
                       {eachData.detailsOfIts}
                     </p>
                   </div>
                 ))}
               </div>
-              <div className="flex flex-col gap-4">
-                <p className="text-[#fff] font-medium text-[20px] tracking-[0.5px]">
+
+              <div className="flex flex-col gap-3 sm:gap-4">
+                <p className="text-[#fff] font-medium text-[17px] sm:text-[20px] tracking-[0.5px]">
                   EDUCATION
                 </p>
 
                 {aboutData?.educationData?.map((eachData) => (
                   <div key={eachData._id} className="mb-2">
-                    <p className="text-primary mb-[5px]">{eachData.year}</p>
-                    <p className="text-[#4D66D0] font-medium text-[20px]">
+                    <p className="text-primary text-[13px] sm:text-[15px] mb-[3px] sm:mb-[5px]">
+                      {eachData.year}
+                    </p>
+                    <p className="text-[#4D66D0] font-medium text-[18px] sm:text-[20px]">
                       {eachData.heading}
                     </p>
-                    <p className="text-primary text-[12px] mt-1">
+                    <p className="text-primary text-[10px] sm:text-[12px] sm:mt-1">
                       {eachData.institution}
                     </p>
-                    <p className="text-primary text-[14px] mt-1">
+                    <p className="text-primary text-[12px] sm:text-[14px] mt-1">
                       {eachData.detailsOfIts}
                     </p>
                   </div>
                 ))}
-
-                <div className="mb-2">
-                  <p className="text-primary mb-[5px]">2007 - 2009</p>
-                  <p className="text-[#4D66D0] font-medium text-[20px]">
-                    Master Degree in Designing
-                  </p>
-                  <p className="text-primary text-[12px] mt-1">
-                    University of Texas
-                  </p>
-                  <p className="text-primary text-[14px] mt-1">
-                    Sit amet luctussd fav venenatis, lectus magna fringilla inis
-                    urna, porttitor rhoncus dolor purus non enim praesent in
-                    elementum sahas facilisis leo, vel fringilla est ullamcorper
-                    eget nulla facilisi etisam dignissim diam quis enim lobortis
-                    viverra orci sagittis eu volutpat odio facilisis mauris sit.
-                  </p>
-                </div>
               </div>
 
-              <div className="flex flex-col gap-4">
-                <p className="text-[#fff] font-medium text-[20px] tracking-[0.5px] mb-2">
+              <div className="flex flex-col gap-3 sm:gap-4">
+                <p className="text-[#fff] font-medium text-[17px] sm:text-[20px] tracking-[0.5px] mb-1 sm:mb-2">
                   SKILLS
                 </p>
 
-                <div className="grid grid-cols-2 gap-8">
+                <div className="grid grid-cols-2 gap-6 sm:gap-8">
                   {aboutData?.skills?.map((eachData) => (
                     <div>
-                      <p className="text-primary mb-[2px] text-[13px]">
+                      <p className="text-primary mb-[1px] sm:mb-[2px] text-[11px] sm:text-[13px]">
                         {eachData?.percentage}
                       </p>
-                      <p className="text-accent font-medium text-[20px]">
+                      <p className="text-accent font-medium text-[18px] sm:text-[20px]">
                         {eachData?.name}
                       </p>
-                      <p className="text-primary text-[12px] mt-1">
+                      <p className="text-primary text-[10px] sm:text-[12px] mt-1">
                         {eachData?.quality}
                       </p>
                     </div>
